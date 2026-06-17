5e6049cc06c95a33df6e4ecb54057000c6e86ece
Gienix <96149117+Gienixx@users.noreply.github.com>
2026-06-18 03:21:13 +0800

    Update supabaseClient.js

--- scripts/supabaseClient.js
+++ scripts/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY'
+const supabaseUrl = 'https://kfhyckyrgplkqhsbuwnx.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmaHlja3lyZ3Bsa3Foc2J1d254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MzQwMzAsImV4cCI6MjA5NzIxMDAzMH0.fx_VADGD6VWoRjV_Sk25rMVrVjWCiYugw2oYS2D8Rpo'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
