c68efed1612c483adfeac9410f81444097930a4d
Gienix <96149117+Gienixx@users.noreply.github.com>
2026-06-18 03:39:01 +0800

    Update data.js

--- functions/api/data.js
+++ functions/api/data.js
@@ -1,12 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-export async function onRequest(context) {
-  const supabase = createClient(
-    context.env.https://kfhyckyrgplkqhsbuwnx.supabase.co,
-    context.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmaHlja3lyZ3Bsa3Foc2J1d254Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTYzNDAzMCwiZXhwIjoyMDk3MjEwMDMwfQ.ViKwUMvn_-RPIcohWriFadpp0HD6fyxWGz2nAbAZhPY
-  )
+const supabaseUrl = 'https://kfhyckyrgplkqhsbuwnx.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmaHlja3lyZ3Bsa3Foc2J1d254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MzQwMzAsImV4cCI6MjA5NzIxMDAzMH0.fx_VADGD6VWoRjV_Sk25rMVrVjWCiYugw2oYS2D8Rpo'
 
-  const { data, error } = await supabase.from('login').select('*')
-
-  return Response.json({ data, error })
-}
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+)
