f78ca3351171b822a7d1f7cc0efac7faf5759776
Gienix <96149117+Gienixx@users.noreply.github.com>
2026-06-18 03:36:26 +0800

    Update data.js

--- functions/api/data.js
+++ functions/api/data.js
@@ -2,11 +2,11 @@ import { createClient } from '@supabase/supabase-js'
 
 export async function onRequest(context) {
   const supabase = createClient(
-    context.env.SUPABASE_URL,
-    context.env.SUPABASE_SERVICE_ROLE_KEY
+    context.env.https://kfhyckyrgplkqhsbuwnx.supabase.co,
+    context.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmaHlja3lyZ3Bsa3Foc2J1d254Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTYzNDAzMCwiZXhwIjoyMDk3MjEwMDMwfQ.ViKwUMvn_-RPIcohWriFadpp0HD6fyxWGz2nAbAZhPY
   )
 
-  const { data, error } = await supabase.from('your_table').select('*')
+  const { data, error } = await supabase.from('login').select('*')
 
   return Response.json({ data, error })
 }
