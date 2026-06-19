cf8f4018a4aae0b9b2953fd930f46d758b31e914
Gienix <96149117+Gienixx@users.noreply.github.com>
2026-06-20 03:48:38 +0800

    Update change-password.js

--- functions/change-password.js
+++ functions/change-password.js
@@ -1,21 +1,15 @@
-function jsonResponse(
-data,
-status = 200
-) {
-return new Response(
-JSON.stringify(data),
-{
-status,
-headers: {
-'Content-Type':
-'application/json; charset=utf-8',
-
-```
-    'Cache-Control':
-      'no-store'
-  }
+function jsonResponse(data, status = 200) {
+  return new Response(
+    JSON.stringify(data),
+    {
+      status,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Cache-Control': 'no-store'
+      }
+    }
+  )
 }
-```
 
 )
 }
