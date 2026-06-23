8c3b5c448e0796d7a985cf59a7559cac5758f59c
Gienix <96149117+Gienixx@users.noreply.github.com>
2026-06-23 20:42:22 +0800

    Remove unused invite endpoint scaffold

--- functions/invite-user.js
+++
@@ -1,6 +0,0 @@
-export async function onRequestPost(context) {
-  return new Response(JSON.stringify({ error: 'Not implemented' }), {
-    status: 501,
-    headers: { 'Content-Type': 'application/json' }
-  })
-}
